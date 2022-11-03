8dcac2b06ba4beef7f60df069efa0b8191ef656c
Joel Maciel <soldado25461@gmail.com>
2022-11-03 20:17:49 -0300

    feat: join and SUM queries using JPQL and SQL

--- uri2609/src/main/java/com/sql/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/sql/uri2609/Uri2609Application.java
@@ -1,10 +1,15 @@
 package com.sql.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sql.uri2609.dto.CategorySumDTO;
+import com.sql.uri2609.projections.CategorySumProjection;
 import com.sql.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -19,6 +24,35 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		List<CategorySumProjection> list = repository.searchSQL();
+		List<CategorySumDTO> categoryDto = list.stream()
+				.map(category -> new CategorySumDTO(category)).collect(Collectors.toList());
+		
+		System.out.println("\n******  ROOT SQL RESULT  ******** \n");
+		
+		for (CategorySumDTO category : categoryDto) {
+			System.out.println(category);
+		}
+		
+		System.out.println("\n\n");
+		
+		List<CategorySumDTO> categories = repository.searchJPQL();
+		
+		System.out.println("\n******  ROOT JPQL RESULT  ******** \n");
+		
+		for (CategorySumDTO category : categories) {
+			System.out.println(category);
+		}
+		
 
 	}
 }
+
+
+
+
+
+
+
+
--- uri2609/src/main/java/com/sql/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/sql/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,26 @@
 package com.sql.uri2609.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sql.uri2609.dto.CategorySumDTO;
 import com.sql.uri2609.entities.Category;
+import com.sql.uri2609.projections.CategorySumProjection;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	@Query(nativeQuery = true, value = "select categories.name, sum(products.amount) "
+			+ "from categories "
+			+ "inner join products on products.id_categories = categories.id "
+			+ "group by categories.name")
+	List<CategorySumProjection> searchSQL();
+	
+	
+	@Query("select new com.sql.uri2609.dto.CategorySumDTO(p.category.name, sum(p.amount)) "
+			+ "from Product p "
+			+ "group by p.category.name")
+	List<CategorySumDTO> searchJPQL();
 
 }
