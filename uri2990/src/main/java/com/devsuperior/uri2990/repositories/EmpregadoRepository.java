07a528ddd63e2bc4635e3d20725419298e5c144e
Joel Maciel <soldado25461@gmail.com>
2022-11-04 10:58:00 -0300

    feat: left join com sql e jpql

--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,37 @@
 package com.devsuperior.uri2990.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import com.devsuperior.uri2990.entities.Empregado;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+	
+	
+	@Query(nativeQuery = true, value = "select empregados.cpf, empregados.enome, departamentos.dnome "
+			+ "from empregados "
+			+ "inner join departamentos on empregados.dnumero = departamentos.dnumero "
+			+ "where empregados.cpf not in ( "
+			+ "	select empregados.cpf "
+			+ "	from empregados "
+			+ "	inner join trabalha on trabalha.cpf_emp = empregados.cpf "
+			+ ") "
+			+ "order by empregados.cpf")
+	List<EmpregadoDeptProjection> searchSQL();
+	
+	
+	@Query( "select new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(emp.cpf, emp.enome, emp.departamento.dnome) "
+			+ "from Empregado emp "
+			+ "where emp.cpf not in ( "
+			+ "	select emp.cpf "
+			+ "	from Empregado emp"
+			+ "	inner join emp.projetosOndeTrabalha "
+			+ ") "
+			+ "order by emp.cpf")
+	List<EmpregadoDeptDTO> searchJPQL();
 
 }
